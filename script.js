4cb3be2ae45d139470391d37e6ce91c34ec4e982
JoeOrtiz2003 <127639367+JoeOrtiz2003@users.noreply.github.com>
2025-06-04 18:09:40 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -63,9 +63,9 @@ async function fetchAndDisplayData() {
             `;
 
             // Assign rows to left or right grouping based on rank
-            if (index < 8) {
+            if (index < 9) {
                 leftGrouping.appendChild(teamBraket); // Ranks 1 to 8
-            } else if (index >= 8 && index < 16) {
+            } else if (index >= 8 && index < 18) {
                 rightGrouping.appendChild(teamBraket); // Ranks 9 to 16
             }
         });
