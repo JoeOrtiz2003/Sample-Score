5786f1c3a47fefe52ade76f28e153e2ada5b60af
JoeOrtiz2003 <127639367+JoeOrtiz2003@users.noreply.github.com>
2025-06-08 16:38:34 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -63,9 +63,9 @@ async function fetchAndDisplayData() {
             `;
 
             // Assign rows to left or right grouping based on rank
-            if (index < 9) {
+            if (index < 8) {
                 leftGrouping.appendChild(teamBraket); // Ranks 1 to 8
-            } else if (index >= 8 && index < 18) {
+            } else if (index >= 8 && index < 16) {
                 rightGrouping.appendChild(teamBraket); // Ranks 9 to 16
             }
         });
@@ -79,4 +79,3 @@ async function fetchAndDisplayData() {
 // Call the function to fetch and display the data every 5 seconds
 fetchAndDisplayData(); // Initial fetch
 setInterval(fetchAndDisplayData, 5000); // Auto-fetch every 5 seconds
-
